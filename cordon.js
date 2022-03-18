01571f9dbaf9ffa0787004650aa9821a718df26b
Pavel <100792228+PavelMaloi@users.noreply.github.com>
2022-03-18 18:44:44 +0300

    Create cordon.js

---
+++ cordon.js
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Security-Policy" content="default-src 'none'; object-src 'none'; script-src resource: data: blob: chrome;">
+    <meta name="color-scheme" content="light dark">
+    <title data-l10n-id="newtab-page-title"></title>
+    <link rel="icon" type="image/png" href="chrome:/branding>
+    <link rel="localization" href="branding/brand.ftl" />
+    <link rel="localization" href="browser/branding.ftl" />
+    <link rel="localization" href="browser/newtab/newtab.ftl" />
+    <link rel="stylesheet" href="chrome://browser/contentSearchUI.css" />
+    <link rel="stylesheet" href="chrome://activity-stream.css" />
+  </head>
+  <body class="activity-stream">
+    <div id="root"></div>
+    <div id="footer-asrouter-container" role="presentation">>
+  </body>
+</html>
